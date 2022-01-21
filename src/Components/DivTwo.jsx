cfef03013abfc7f30d11e7e853723ac39eb4ad77
Robert <rhbanks779@gmail.com>
2022-01-21 14:20:48 -0500

    second

---
+++ src/Components/DivTwo.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import DivThree from './DivThree';
+
+class DivTwo extends Component {
+    render() {
+        return (
+            <div>
+                <DivThree />
+                <DivThree />
+            </div>
+        );
+    }
+}
+export default DivTwo;
